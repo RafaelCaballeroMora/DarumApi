053a5ba46cd30851266f5d76ff1118fd6f493939
RafaelCaballeroMora <rafaelcaballeromora@gmail.com>
2024-05-19 17:05:52 +0100

    swap prod DB to supabase

--- src/main/resources/db_oracle/04_firebase_integration_rollback.sql
+++
@@ -1,6 +0,0 @@
--- Drop columns in Usuario table
-ALTER TABLE Usuario DROP COLUMN authorities;
-ALTER TABLE Usuario DROP COLUMN firebase_user_id;
-
--- Add column in Usuario table
-ALTER TABLE Usuario ADD (password VARCHAR2(255));
